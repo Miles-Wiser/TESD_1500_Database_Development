0e61ecb2a5a1db4eed005d375d216ec4ae7a605b
Miles-Wiser <129561396+Miles-Wiser@users.noreply.github.com>
2025-05-23 09:04:17 -0600

    Add files via upload

--- Chapter 1/cookies.php
+++ Chapter 1/cookies.php
@@ -9,10 +9,24 @@
 
 <head>
     <title>Mouse Thanks You! But....</title>
+    <style>
+        body {width: 85%; margin: 10px auto;
+             font-family: 'Trebuchet MS', 'Lucida Sans', Arial, sans-serif;}
+        table, th, td {background-color: rgb(242, 230, 245);
+             border: 2px solid rgb(60, 45, 60); border-collapse: collapse;
+             text-align: center;}
+        table th {background-color: rgba(191, 150, 235, 0.30);}
+        th, td {padding: 6px;}
+    </style>
 </head>
 
 <body>
-    <h1>Mouse Says: "Thank You!"</h1>
+    <?php
+    if ($cookiesQty <= 0)
+        echo '<h1>Mouse Says: Happiness is Fleeting</h1>';
+    else
+        echo '<h1>Mouse Says: Thank You!</h1>';
+    ?>
     <table>
         <tr>
             <th>Item</th>
